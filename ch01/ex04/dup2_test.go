c6e36bad8e71a7534d9fb86a07018581c85b5ed6
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-13 01:00:39 +0900

    test: add Dup test

--- ch01/ex04/dup2_test.go
+++ ch01/ex04/dup2_test.go
@@ -78,3 +78,82 @@ func TestCountLines(t *testing.T) {
 		}
 	}
 }
+
+func TestDup(t *testing.T) {
+	t.Run("NilCase", func(t *testing.T) {
+		DefaultSource = bytes.NewBufferString("1\n2\n1\n")
+		expected := map[string][]string{
+			"1": {"<stdin>", "<stdin>"},
+		}
+		got, err := Dup(nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("unexpected result. expected: %v, but got: %v", expected, got)
+		}
+	})
+	t.Run("EmptyFile", func(t *testing.T) {
+		DefaultSource = bytes.NewBufferString("1\n2\n1\n")
+		expected := map[string][]string{
+			"1": {"<stdin>", "<stdin>"},
+		}
+		got, err := Dup([]string{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("unexpected result. expected: %v, but got: %v", expected, got)
+		}
+	})
+	t.Run("FileNotFound", func(t *testing.T) {
+		counts, err := Dup([]string{"notfound"})
+		if err == nil {
+			t.Error("accept invalid filepath")
+		}
+		if counts != nil {
+			t.Error("return invalid result")
+		}
+	})
+	t.Run("PositiveCase", func(t *testing.T) {
+		ts := []struct {
+			files    []string
+			expected map[string][]string
+		}{
+			{
+				files: []string{"testdata/A.txt"},
+				expected: map[string][]string{
+					"aaaaaa": {"testdata/A.txt", "testdata/A.txt"},
+				},
+			},
+			{
+				files: []string{"testdata/A.txt", "testdata/B.txt"},
+				expected: map[string][]string{
+					"aaaaaa": {"testdata/A.txt", "testdata/A.txt"},
+					"hhhh":   {"testdata/A.txt", "testdata/B.txt"},
+				},
+			},
+			{
+				files: []string{"testdata/C.txt", "testdata/A.txt"},
+				expected: map[string][]string{
+					"aaaaaa": {"testdata/A.txt", "testdata/A.txt"},
+					"poyo":   {"testdata/C.txt", "testdata/A.txt"},
+				},
+			},
+			{
+				files:    []string{"testdata/B.txt", "testdata/C.txt"},
+				expected: map[string][]string{},
+			},
+		}
+		for _, tc := range ts {
+			got, err := Dup(tc.files)
+			if err != nil {
+				t.Error(err)
+				continue
+			}
+			if !reflect.DeepEqual(got, tc.expected) {
+				t.Errorf("unexpected result. expected: %v, but got: %v", tc.expected, got)
+			}
+		}
+	})
+}
