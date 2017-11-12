c274d4ec3d0faf6fbd325c6dcc8f7a442ca0b496
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-12 21:20:54 +0900

    test: add CountLines test

--- ch01/ex04/dup2_test.go
+++ ch01/ex04/dup2_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
@@ -30,6 +31,41 @@ func TestUnique(t *testing.T) {
 	for _, tc := range ts {
 		got := Unique(tc.strs)
 		if !reflect.DeepEqual(got, tc.expected) {
+			t.Errorf("unexpected result. expected: %v, but got: %v", tc.expected, got)
+		}
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	ts := []struct {
+		text     string
+		name     string
+		expected map[string][]string
+	}{
+		{
+			text: "1\n2\n3\n4\n\n",
+			name: "test",
+			expected: map[string][]string{
+				"1": {"test"},
+				"2": {"test"},
+				"3": {"test"},
+				"4": {"test"},
+				"":  {"test"},
+			},
+		},
+		{
+			text: "1\n2\n1\n1\n",
+			name: "file.txt",
+			expected: map[string][]string{
+				"1": {"file.txt", "file.txt", "file.txt"},
+				"2": {"file.txt"},
+			},
+		},
+	}
+	for _, tc := range ts {
+		c := make(map[string][]string)
+		CountLines(tc.name, bytes.NewBufferString(tc.text), c)
+		if got := c; !reflect.DeepEqual(got, tc.expected) {
 			t.Errorf("unexpected result. expected: %v, but got: %v", tc.expected, got)
 		}
 	}
