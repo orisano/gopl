a0292a3cc08f7779b442a18a105789333a81dece
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-24 01:16:15 +0900

    feat: add ex02/ex05 another method

--- ch02/ex05/popcount.go
+++ ch02/ex05/popcount.go
@@ -1,10 +1,18 @@
 package popcount
 
 var pc [256]byte
+var pc15 [32 * 1024]byte
+var pc16 [64 * 1024]byte
 
 func init() {
 	for i := range pc {
 		pc[i] = pc[i/2] + byte(i&1)
+	}
+	for i := range pc15 {
+		pc15[i] = pc15[i/2] + byte(i&1)
+	}
+	for i := range pc16 {
+		pc16[i] = pc16[i/2] + byte(i&1)
 	}
 }
 
@@ -26,4 +34,27 @@ func PopCountBitMagic(x uint64) int {
 		cnt++
 	}
 	return cnt
+}
+
+func PopCountParallel(x uint64) int {
+	x = (x&0xAAAAAAAAAAAAAAAA)>>1 + (x & 0x5555555555555555)
+	x = (x&0xCCCCCCCCCCCCCCCC)>>2 + (x & 0x3333333333333333)
+	x = (x&0xF0F0F0F0F0F0F0F0)>>4 + (x & 0x0F0F0F0F0F0F0F0F)
+	x = (x&0xFF00FF00FF00FF00)>>8 + (x & 0x00FF00FF00FF00FF)
+	x = (x&0xFFFF0000FFFF0000)>>16 + (x & 0x0000FFFF0000FFFF)
+	return int((x&0xFFFFFFFF00000000)>>32 + (x & 0x00000000FFFFFFFF))
+}
+
+func PopCount16(x uint64) int {
+	return int(pc16[(x>>(0*16))&0xffff] +
+		pc16[(x>>(1*16))&0xffff] +
+		pc16[(x>>(2*16))&0xffff] +
+		pc16[(x>>(3*16))&0xffff])
+}
+
+func PopCount15(x uint64) int {
+	return int(pc15[(x>>(0*16+1))&0x7fff] + byte((x>>(0*16))&1) +
+		pc15[(x>>(1*16+1))&0x7fff] + byte((x>>(1*16))&1) +
+		pc15[(x>>(2*16+1))&0x7fff] + byte((x>>(2*16))&1) +
+		pc15[(x>>(3*16+1))&0x7fff] + byte((x>>(3*16))&1))
 }
--- ch02/ex05/popcount_test.go
+++ ch02/ex05/popcount_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
+func TestPopCount(t *testing.T) {
+	for i := uint64(0); i < 1000000; i++ {
+		pc := PopCount(i)
+		if got := PopCountBitMagic(i); got != pc {
+			t.Errorf("unexpected BitMagic result. expected: %v, but got: %v", pc, got)
+		}
+		if got := PopCountParallel(i); got != pc {
+			t.Errorf("unexpected Parallel result. expected: %v, but got: %v", pc, got)
+		}
+		if got := PopCount16(i); got != pc {
+			t.Errorf("unexpected 16 result. expected: %v, but got: %v", pc, got)
+		}
+		if got := PopCount15(i); got != pc {
+			t.Errorf("unexpected 15 result. expected: %v, but got: %v", pc, got)
+		}
+	}
+}
+
 func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		x := rand.Uint64()
@@ -16,5 +34,26 @@ func BenchmarkPopCountBitMagic(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		x := rand.Uint64()
 		PopCountBitMagic(x)
+	}
+}
+
+func BenchmarkPopCountParallel(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x := rand.Uint64()
+		PopCountParallel(x)
+	}
+}
+
+func BenchmarkPopCount16(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x := rand.Uint64()
+		PopCount16(x)
+	}
+}
+
+func BenchmarkPopCount15(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		x := rand.Uint64()
+		PopCount15(x)
 	}
 }
