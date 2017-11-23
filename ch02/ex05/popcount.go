89e87a7abe2f1e0a017a9eb0b1540f6d8750189f
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-24 00:33:22 +0900

    feat: add ch02/ex05

---
+++ ch02/ex05/popcount.go
@@ -0,0 +1,29 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountBitMagic(x uint64) int {
+	cnt := 0
+	for x > 0 {
+		x &= x - 1
+		cnt++
+	}
+	return cnt
+}
