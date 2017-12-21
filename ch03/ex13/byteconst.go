6b301fd0144c9804e39f70590b19b53f252853c4
Nao YONASHIRO <owan.orisano@gmail.com>
2017-12-22 02:53:21 +0900

    feat: add ch03/ex13

---
+++ ch03/ex13/byteconst.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const (
+	B  = 1
+	KB = 1000 * B
+	MB = 1000 * KB
+	GB = 1000 * MB
+	TB = 1000 * GB
+	PB = 1000 * TB
+	EB = 1000 * PB
+	ZB = 1000 * EB
+	YB = 1000 * ZB
+)
+
+func main() {
+	fmt.Println(B)
+	fmt.Println(KB)
+	fmt.Println(MB)
+	fmt.Println(GB)
+	fmt.Println(TB)
+	fmt.Println(PB)
+	fmt.Println(EB)
+}
