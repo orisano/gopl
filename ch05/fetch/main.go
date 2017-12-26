c86e81ef9464e3f89281504f99c4474c097a5f18
Nao YONASHIRO <owan.orisano@gmail.com>
2017-12-27 05:42:25 +0900

    feat: add ch05/fetch

---
+++ ch05/fetch/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		filename, n, err := fetch(url)
+		if err != nil {
+			fmt.Println("fetch:", err)
+			continue
+		}
+		fmt.Println(filename, n, "bytes")
+	}
+}
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	return local, n, err
+}
