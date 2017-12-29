9db1ea171d1098bae609be529cdcc73d5dceaef8
Nao YONASHIRO <owan.orisano@gmail.com>
2017-12-30 03:45:12 +0900

    feat: add Point.ScaleBy

--- ch06/geometry/point.go
+++ ch06/geometry/point.go
@@ -11,3 +11,8 @@ func Distance(p, q Point) float64 {
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
+
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
