ca568f75d8e89c81cf4fc50dbb7ab1b8de49c9ce
Nao YONASHIRO <owan.orisano@gmail.com>
2017-12-31 03:36:59 +0900

    feat: update ch06/ex05

--- ch06/ex05/intset/intset.go
+++ ch06/ex05/intset/intset.go
@@ -6,7 +6,9 @@ import (
 	"math/bits"
 )
 
-const bitSize = 32 << (^uint(0) >> 63)
+const (
+	bitSize = 32 << (^uint(0) >> 63)
+)
 
 type IntSet struct {
 	words []uint
@@ -25,6 +27,12 @@ func (s *IntSet) Add(x int) {
 	s.words[word] |= 1 << bit
 }
 
+func (s *IntSet) AddAll(xs ...int) {
+	for _, x := range xs {
+		s.Add(x)
+	}
+}
+
 func (s *IntSet) UnionWith(t *IntSet) {
 	for i, tword := range t.words {
 		if i < len(s.words) {
@@ -32,6 +40,54 @@ func (s *IntSet) UnionWith(t *IntSet) {
 		} else {
 			s.words = append(s.words, tword)
 		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < bitSize; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", bitSize*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func (s *IntSet) Len() int {
+	sum := 0
+	for _, word := range s.words {
+		sum += bits.OnesCount(word)
+	}
+	return sum
+}
+
+func (s *IntSet) Remove(x int) {
+	word, bit := x/bitSize, uint(x%bitSize)
+	if word >= len(s.words) {
+		return
+	}
+	s.words[word] &^= 1 << bit
+}
+
+func (s *IntSet) Clear() {
+	for i := range s.words {
+		s.words[i] = 0
+	}
+}
+
+func (s *IntSet) Copy() *IntSet {
+	return &IntSet{
+		words: append([]uint(nil), s.words...),
 	}
 }
 
@@ -65,54 +121,6 @@ func (s *IntSet) SymmetricDifference(t *IntSet) {
 		} else {
 			s.words = append(s.words, tword)
 		}
-	}
-}
-
-func (s *IntSet) String() string {
-	var buf bytes.Buffer
-	buf.WriteByte('{')
-	for i, word := range s.words {
-		if word == 0 {
-			continue
-		}
-		for j := 0; j < bitSize; j++ {
-			if word&(1<<uint(j)) != 0 {
-				if buf.Len() > len("{") {
-					buf.WriteByte(' ')
-				}
-				fmt.Fprint(&buf, "%d", bitSize*i+j)
-			}
-		}
-	}
-	buf.WriteByte('}')
-	return buf.String()
-}
-
-func (s *IntSet) Len() int {
-	sum := 0
-	for _, word := range s.words {
-		sum += bits.OnesCount(word)
-	}
-	return sum
-}
-
-func (s *IntSet) Remove(x int) {
-	word, bit := x/bitSize, uint(x%bitSize)
-	if word >= len(s.words) {
-		return
-	}
-	s.words[word] &^= 1 << bit
-}
-
-func (s *IntSet) Clear() {
-	for i := range s.words {
-		s.words[i] = 0
-	}
-}
-
-func (s *IntSet) Copy() *IntSet {
-	return &IntSet{
-		words: append(nil, s.words...),
 	}
 }
 
