b9fa6d8c2b8983e15d23125f47d7feaff6471f01
Nao YONASHIRO <owan.orisano@gmail.com>
2017-12-31 03:31:57 +0900

    chore: update ch06/ex04

--- ch06/ex04/intset/intset.go
+++ ch06/ex04/intset/intset.go
@@ -23,6 +23,12 @@ func (s *IntSet) Add(x int) {
 	s.words[word] |= 1 << bit
 }
 
+func (s *IntSet) AddAll(xs ...int) {
+	for _, x := range xs {
+		s.Add(x)
+	}
+}
+
 func (s *IntSet) UnionWith(t *IntSet) {
 	for i, tword := range t.words {
 		if i < len(s.words) {
@@ -30,6 +36,54 @@ func (s *IntSet) UnionWith(t *IntSet) {
 		} else {
 			s.words = append(s.words, tword)
 		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func (s *IntSet) Len() int {
+	sum := 0
+	for _, word := range s.words {
+		sum += bits.OnesCount64(word)
+	}
+	return sum
+}
+
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word >= len(s.words) {
+		return
+	}
+	s.words[word] &^= 1 << bit
+}
+
+func (s *IntSet) Clear() {
+	for i := range s.words {
+		s.words[i] = 0
+	}
+}
+
+func (s *IntSet) Copy() *IntSet {
+	return &IntSet{
+		words: append([]uint64(nil), s.words...),
 	}
 }
 
@@ -63,54 +117,6 @@ func (s *IntSet) SymmetricDifference(t *IntSet) {
 		} else {
 			s.words = append(s.words, tword)
 		}
-	}
-}
-
-func (s *IntSet) String() string {
-	var buf bytes.Buffer
-	buf.WriteByte('{')
-	for i, word := range s.words {
-		if word == 0 {
-			continue
-		}
-		for j := 0; j < 64; j++ {
-			if word&(1<<uint(j)) != 0 {
-				if buf.Len() > len("{") {
-					buf.WriteByte(' ')
-				}
-				fmt.Fprint(&buf, "%d", 64*i+j)
-			}
-		}
-	}
-	buf.WriteByte('}')
-	return buf.String()
-}
-
-func (s *IntSet) Len() int {
-	sum := 0
-	for _, word := range s.words {
-		sum += bits.OnesCount64(word)
-	}
-	return sum
-}
-
-func (s *IntSet) Remove(x int) {
-	word, bit := x/64, uint(x%64)
-	if word >= len(s.words) {
-		return
-	}
-	s.words[word] &^= 1 << bit
-}
-
-func (s *IntSet) Clear() {
-	for i := range s.words {
-		s.words[i] = 0
-	}
-}
-
-func (s *IntSet) Copy() *IntSet {
-	return &IntSet{
-		words: append(nil, s.words...),
 	}
 }
 
