426fca6b3675a3cb78902074a2e24be802580ffe
Nao YONASHIRO <owan.orisano@gmail.com>
2018-01-04 01:26:04 +0900

    feat: add ch07/tempconv

---
+++ ch07/tempconv/flag.go
@@ -0,0 +1,29 @@
+package tempconv
+
+import (
+	"flag"
+	"fmt"
+)
+
+type celsiusFlag struct{ Celsius }
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
