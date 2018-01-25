9825b91008e67ba348e72df03be315689dd9f544
Nao YONASHIRO <owan.orisano@gmail.com>
2018-01-25 23:42:50 +0900

    feat: add ch08/thumbnail

---
+++ ch08/thumbnail/cmd/thumbnail/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/orisano/gopl/ch08/thumbnail"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		thumb, err := thumbnail.ImageFile(input.Text())
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(thumb)
+	}
+	if err := input.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
