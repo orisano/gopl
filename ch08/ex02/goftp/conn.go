1f0cbec905871f983a11647e79e881921c1a670c
Nao YONASHIRO <owan.orisano@gmail.com>
2018-02-10 22:32:22 +0900

    refactor: refactoring Server

---
+++ ch08/ex02/goftp/conn.go
@@ -0,0 +1,74 @@
+package goftp
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/orisano/gopl/ch08/ex02/goftp/ftpcodes"
+)
+
+type Conn struct {
+	logger           *log.Logger
+	conn             net.Conn
+	fs               FileSystem
+	workingDirectory string
+}
+
+func (c *Conn) Handle() {
+	defer c.conn.Close()
+
+	c.writeReply(ftpcodes.ServiceReadyForNewUser)
+
+	scanner := bufio.NewScanner(c.conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		c.logger.Print(">>>> ", line)
+
+		tokens := strings.Split(line, " ")
+		cmd, args := tokens[0], tokens[1:]
+		next := c.runCommand(cmd, args)
+		if !next {
+			break
+		}
+	}
+}
+
+func (c *Conn) runCommand(cmd string, args []string) bool {
+	switch strings.ToLower(cmd) {
+	case "user":
+		c.writeReply(ftpcodes.UserLoggedOn)
+	case "quit":
+		c.writeReply(ftpcodes.ServiceClosingTELNETConnection)
+		return false
+	case "syst":
+		c.writeReply(ftpcodes.SystemType)
+	default:
+		c.writeReply(ftpcodes.CommandNotImplemented)
+	}
+	return true
+}
+
+func (c *Conn) writeReply(code int) error {
+	write := func(msg string) error {
+		_, err := fmt.Fprint(c.conn, code, " ", msg, "\r\n")
+		return err
+	}
+	switch code {
+	case ftpcodes.ServiceReadyForNewUser:
+		return write("Service ready")
+	case ftpcodes.CommandNotImplemented:
+		return write("Command not implemented")
+	case ftpcodes.UserLoggedOn:
+		return write("User logged on, proceed")
+	case ftpcodes.ServiceClosingTELNETConnection:
+		return write("Service closing TELNET connection (logged off if appropriate)")
+	case ftpcodes.SystemType:
+		// https://www.iana.org/assignments/operating-system-names/operating-system-names.txt
+		return write("OSX system type")
+	default:
+		panic(fmt.Sprintf("unknown code: %v", code))
+	}
+}
--- ch08/ex02/goftp/server.go
+++ ch08/ex02/goftp/server.go
@@ -1,16 +1,10 @@
 package goftp
 
 import (
-	"bufio"
-	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
-	"strings"
-
-	"github.com/orisano/gopl/ch08/ex02/goftp/ftpcodes"
 )
 
 type Server struct {
@@ -26,42 +20,14 @@ func (s *Server) Serve(l net.Listener) error {
 			s.Logger.Printf("failed to accept: %v", err)
 			continue
 		}
-		go s.handleConn(conn)
-	}
-}
-
-func (s *Server) handleConn(conn net.Conn) {
-	defer conn.Close()
-
-	writeReply(conn, ftpcodes.ServiceReadyForNewUser)
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		line := scanner.Text()
-		s.Logger.Print(">>>> ", line)
-
-		tokens := strings.Split(line, " ")
-		cmd, args := tokens[0], tokens[1:]
-		next := s.runCommand(conn, cmd, args)
-		if !next {
-			break
+		c := &Conn{
+			logger:           s.Logger,
+			conn:             conn,
+			fs:               s.FileSystem,
+			workingDirectory: "/",
 		}
+		go c.Handle()
 	}
-}
-
-func (s *Server) runCommand(w io.Writer, cmd string, args []string) bool {
-	switch strings.ToLower(cmd) {
-	case "user":
-		writeReply(w, ftpcodes.UserLoggedOn)
-	case "quit":
-		writeReply(w, ftpcodes.ServiceClosingTELNETConnection)
-		return false
-	case "syst":
-		writeReply(w, ftpcodes.SystemType)
-	default:
-		writeReply(w, ftpcodes.CommandNotImplemented)
-	}
-	return true
 }
 
 func Serve(l net.Listener, fs FileSystem) error {
@@ -82,26 +48,4 @@ func ListenAndServe(addr string, fs FileSystem) error {
 		return err
 	}
 	return Serve(l, fs)
-}
-
-func writeReply(w io.Writer, code int) error {
-	write := func(msg string) error {
-		_, err := fmt.Fprint(w, code, " ", msg, "\r\n")
-		return err
-	}
-	switch code {
-	case ftpcodes.ServiceReadyForNewUser:
-		return write("Service ready")
-	case ftpcodes.CommandNotImplemented:
-		return write("Command not implemented")
-	case ftpcodes.UserLoggedOn:
-		return write("User logged on, proceed")
-	case ftpcodes.ServiceClosingTELNETConnection:
-		return write("Service closing TELNET connection (logged off if appropriate)")
-	case ftpcodes.SystemType:
-		// https://www.iana.org/assignments/operating-system-names/operating-system-names.txt
-		return write("OSX system type")
-	default:
-		panic(fmt.Sprintf("unknown code: %v", code))
-	}
 }
