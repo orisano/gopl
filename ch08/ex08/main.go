06447546aacf7db60b5873f2fe8f0acddfb370eb
Nao YONASHIRO <owan.orisano@gmail.com>
2018-02-23 03:08:36 +0900

    feat: add ch08/ex08

---
+++ ch08/ex08/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	input := bufio.NewScanner(c)
+
+	lineCh := make(chan string)
+	go func() {
+		for input.Scan() {
+			lineCh <- input.Text()
+		}
+	}()
+
+	for {
+		select {
+		case line, ok := <-lineCh:
+			if !ok {
+				return
+			}
+			go echo(c, line, 1*time.Second)
+		case <-time.After(10 * time.Second):
+			return
+		}
+	}
+
+}
