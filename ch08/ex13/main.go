8aec18c4c8f2b4523f29a8d16f4bd91214cd3b30
Nao YONASHIRO <owan.orisano@gmail.com>
2018-02-23 04:35:07 +0900

    feat: add ch08/ex13

---
+++ ch08/ex13/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+type client chan<- string
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string)
+)
+
+func broadcaster() {
+	clients := make(map[client]bool)
+	for {
+		select {
+		case msg := <-messages:
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	text := make(chan string)
+	go func() {
+		for input.Scan() {
+			text <- input.Text()
+		}
+		close(text)
+	}()
+
+	defer func() {
+		leaving <- ch
+		messages <- who + " has left"
+		conn.Close()
+	}()
+
+	for {
+		select {
+		case txt, ok := <-text:
+			if !ok {
+				return
+			}
+			messages <- who + ": " + txt
+		case <-time.After(5 * time.Minute):
+			return
+		}
+	}
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
