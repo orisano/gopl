1a09ebbffb9712856bd67b810b0b8fc8d9283193
Nao YONASHIRO <owan.orisano@gmail.com>
2018-02-23 05:03:34 +0900

    feat: add ch09/bank3

---
+++ ch09/bank3/bank.go
@@ -0,0 +1,20 @@
+package bank
+
+import "sync"
+
+var (
+	mu      sync.Mutex
+	balance int
+)
+
+func Deposit(amount int) {
+	mu.Lock()
+	balance = balance + amount
+	mu.Unlock()
+}
+func Balance() int {
+	mu.Lock()
+	b := balance
+	mu.Unlock()
+	return b
+}
