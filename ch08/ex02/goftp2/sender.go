29213bd6ef14685090d773326c131d195ea49ff3
Nao YONASHIRO <owan.orisano@gmail.com>
2018-03-07 01:23:51 +0900

    feat: add Sender utilities

--- ch08/ex02/goftp2/sender.go
+++ ch08/ex02/goftp2/sender.go
@@ -1,19 +1,35 @@
 package goftp2
 
+import "fmt"
+
 type Sender interface {
 	Send(code int, message string) error
 }
 
+func CommandOK(s Sender) error {
+	return s.Send(200, "Command Okay")
+}
+
+func PathCreated(s Sender, path string) error {
+	return s.Send(257, fmt.Sprintf("%q created", path))
+}
+
+// This may be a reply to any command if the service knows it must shut down.
+func ServiceNotAvailable(s Sender) error {
+	return s.Send(421, "Service not available, closing control connection")
+}
+
+// This may include errors such as command line too long.
+func CommandUnrecognized(s Sender) error {
+	return s.Send(500, "Syntax error, command unrecognized and the requested action did not take place")
+}
+
+func InvalidParametersOrArguments(s Sender) error {
+	return s.Send(501, "Syntax error in parameters or arguments.")
+}
+
 func CommandNotImplemented(s Sender) error {
 	return s.Send(502, "Command not implemented")
-}
-
-func CommandOK(s Sender) error {
-	return s.Send(200, "Command OK")
-}
-
-func CommandSyntaxError(s Sender) error {
-	return s.Send(500, "Command syntax error")
 }
 
 func CommandNotImplementedForParameter(s Sender) error {
