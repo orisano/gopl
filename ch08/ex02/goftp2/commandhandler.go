a2f667bb2ec8ddeb13b726aa2425bc3567b13749
Nao YONASHIRO <owan.orisano@gmail.com>
2018-03-14 23:48:51 +0900

    docs: add RETR error codes

--- ch08/ex02/goftp2/commandhandler.go
+++ ch08/ex02/goftp2/commandhandler.go
@@ -45,24 +45,36 @@ func NewCommandMux() *CommandMux {
 func DefaultCommandMux() *CommandMux {
 	mux := NewCommandMux()
 
+	// 125: Data connection already open; transfer starting.
+	// 150: File status okay; about to open data connection.
 	// 200: The requested action has been successfully completed.
 	// 215:
 	// 	NAME system type.
 	// 	Where NAME is an official system name from the registry kept by IANA.
 	// 220: Service ready for new user.
 	// 221: Service closing control connection.
+	// 226: Closing data connection.
+	//   Requested file action successful (for example, file transfer or file abort).
 	// 230: User logged in, proceed. Logged out if appropriate.
+	// 250: Requested file action okay, completed.
 	// 257: "PATHNAME" created.
 	// 331: User name okay, need password.
 	// 332: Need account for login.
 	// 421:
 	// 	Service not available, closing control connection.
 	// 	This may be a reply to any command if the service knows it must shut down.
+	// 425: Can't open data connection.
+	// 426: Connection closed; transfer aborted.
+	// 450:
+	//  Requested file action not taken.
+	//  File unavailable (e.g., file busy).
+	// 451: Requested action aborted: local error in processing.
 	// 500:
 	// 	Syntax error, command unrecognized and the requested action did not take place.
 	// 	This may include errors such as command line too long.
 	// 501: Syntax error in parameters or arguments.
 	// 502: Command not implemented.
+	// 530: Not logged in.
 	// 550: Requested action not taken. File unavailable (e.g., file not found, no access).
 
 	// returns: 230, 530, 500, 501, 421, 331, 332
@@ -121,6 +133,10 @@ func DefaultCommandMux() *CommandMux {
 
 	mux.OnFunc("NOOP", func(ctx *Context) {
 		CommandOK(ctx)
+	})
+
+	// returns: 125, 150, 226, 250, 421, 425, 426, 450, 451, 500, 501, 530, 550
+	mux.OnFunc("RETR", func(ctx *Context) {
 	})
 
 	return mux
