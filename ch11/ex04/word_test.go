4f306a4c8f1f4c553b4cf7202693982537b4ae92
Nao YONASHIRO <owan.orisano@gmail.com>
2018-03-23 04:23:27 +0900

    feat: add ch11/ex04

---
+++ ch11/ex04/word_test.go
@@ -0,0 +1,90 @@
+package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{"", true},
+		{"a", true},
+		{"aa", true},
+		{"ab", false},
+		{"kayak", true},
+		{"detartrated", true},
+		{"A man, a plan, a canal: Panama", true},
+		{"Evil I did dwell; lewd did I live.", true},
+		{"Able was I ere I saw Elba", true},
+		{"été", true},
+		{"Et se resservir, ivresse reste.", true},
+		{"palindrome", false},
+		{"desserts", false},
+	}
+
+	for _, test := range tests {
+		if got := IsPalindrome(test.input); got != test.want {
+			t.Errorf(`IsPalindrome(%q) = %v`, test.input, got)
+		}
+	}
+}
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+
+	if len(runes) == 0 {
+		return ""
+	}
+
+	sp := rng.Intn(3)
+	for i := 0; i < sp; i++ {
+		x := rng.Intn(len(runes))
+		runes = append(runes, 0)
+		copy(runes[x+1:], runes[x:])
+		runes[x] = ' '
+	}
+
+	sep := rng.Intn(3)
+	for i := 0; i < sep; i++ {
+		x := rng.Intn(len(runes))
+		runes = append(runes, 0)
+		copy(runes[x+1:], runes[x:])
+		runes[x] = ','
+	}
+
+	return string(runes)
+}
+
+func randomNotPalindrome(rng *rand.Rand) string {
+	s := randomPalindrome(rng)
+	return "54321a" + s + "b12345"
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+	for i := 0; i < 1000; i++ {
+		p := randomNotPalindrome(rng)
+		if IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = true", p)
+		}
+	}
+}
